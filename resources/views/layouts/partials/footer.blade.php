16887af486821815fba3d2d75f31d5cfdc4f24fa
SaKu <sakuofficial@gmail.com>
2021-02-09 03:12:55 +0530

    Remove files

--- resources/views/layouts/partials/footer.blade.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-      <b>Version</b> 1.0
-    </div>
-    <strong>Copyright &copy; 2021 <a href="https://codezilla.lk" target="_blank">Codezilla</a>.</strong> All rights reserved.
-  </footer>
