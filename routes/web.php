fb02f77c40ce2c2e4e0f84377b502eb37b3b1191
SaKu <sakuofficial@gmail.com>
2021-02-09 03:17:06 +0530

    Add new files

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::prefix('admin')->group(function () {
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::resource('products', 'ProductController');
+    Route::get('qr', function () {
+        return view('products.qrcode');
+    });
+});
